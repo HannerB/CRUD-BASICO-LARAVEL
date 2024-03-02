04fcb8e5dd711f4d40256c67155ca439687b7f45
Hanner <Hannerb48@gmail.com>
2024-03-02 14:58:13 -0500

    Definiendo y ejecutando migraciones

--- database/migrations/2024_03_02_180016_create_clients_table.php
+++ database/migrations/2024_03_02_180016_create_clients_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name',75);
+            $table->float('due')->default((0));
+            $table->text('comments')->default('');
             $table->timestamps();
         });
     }
