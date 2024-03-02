62fbbbff1e1e31d7b4200266e4d1cbbeb79ac12a
Hanner <Hannerb48@gmail.com>
2024-03-02 18:36:59 -0500

    Conectando vistas y controlador (implementando routes)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClientController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('client', ClientController::class);
