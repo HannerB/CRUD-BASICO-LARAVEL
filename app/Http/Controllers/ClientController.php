37eeef94166eadef765dfccf1b0e8686e76a4776
Hanner <Hannerb48@gmail.com>
2024-03-03 16:10:35 -0500

    Variables de sesión

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ClientController extends Controller
 {
@@ -35,6 +36,8 @@ class ClientController extends Controller
 
         $client = Client::create($request->only('name','due','comments'));
 
+        Session::flash('mensaje', 'Registro creado con Éxito!');
+        
         return redirect()->route('client.index');
     }
 
